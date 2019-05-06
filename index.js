e321dcf95a4d78dde1f3bfe140cb9f5b99b953cf
Chubbard022 <chubbard022@gmail.com>
2019-05-06 13:35:34 -0700

    protected middleware finished, index.js bugs fixed

--- index.js
+++ index.js
@@ -1,46 +1,65 @@
-const express = require("express")
-const helmet = require("helmet")
-const cors = require("cors")
-const bcrypt = require("bcryptjs")
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs'); 
+const port = 5000;
 
-const db = require("./database/dbConfig")
-const Users = require("./routes/route-model")
-const protected = require("./auth/protected-middleware")
+const Users = require('./routes/route-model')
+const protected = require('./auth/protected-middleware')
 
-const server = express()
-const PORT = 5000;
+const server = express();
 
- server.use(helmet())
- server.use(express.json())
- server.use(cors())
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
 
-server.get("/",(req,res)=>{
-    res.send("server working")
-})
+server.get('/', (req, res) => {
+  res.send("It's alive!");
+});
 
-server.post("/api/register",(req,res)=>{
-    const user = req.body;
-    const hash = bcrypt.hashSync(user.password,8)
+server.post('/api/register', (req, res) => {
+  let user = req.body;
+  // check for username and password
 
-    user.password = hash;
+  const hash = bcrypt.hashSync(user.password, 8); // 2^8 rounds <<<<<<<<<<<<
+  // pass > hashit > hash > hashit > hash > hashit > hash
+  user.password = hash; // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-    Users.add(user)
-    .then(response =>{
-        res.status(201).json(response)
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
     })
-    .catch(err =>{
-        res.status(500).json(err)
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.post('/api/login', (req, res) => {
+  let { username, password } = req.body;
+
+  // we compare the password guess against the database hash
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      //
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
     })
-})
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
+// protect this route, users must provide valid username/password to see the list of users
+server.get('/api/users', protected, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
 
-
-
-
-
-
-
-
-server.listen(PORT,()=>{
-    console.log(`server listening on http://localhost:${PORT}`);
-})
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
