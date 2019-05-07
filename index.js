bff3f80e98160420a9949eb4b259c6c39e268cc0
Chubbard022 <chubbard022@gmail.com>
2019-05-07 10:37:47 -0700

    express-sessions up and running

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const port = 5000;
+const session = require("express-session")
 
 const loginRouter = require("./routes/loginRoute")
 const registerRouter = require("./routes/registerRoute")
 const usersRouter = require("./routes/userRoute")
-
-
-
 const server = express();
 
+const sessionConfig = {
+    name: "cookies",
+    secret: "keep this a secret",
+    cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60,
+        secure: false
+    },
+    resave: true,
+    saveUninitialized: true
+}
+
+server.use(session(sessionConfig))
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
