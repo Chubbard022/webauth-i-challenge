6a4f016691bdde304800d4f062dd822314a1179e
Chubbard022 <chubbard022@gmail.com>
2019-05-07 11:00:28 -0700

    changed middleware to look at sessions rather than bcryptjs

--- auth/protected-middleware.js
+++ auth/protected-middleware.js
@@ -1,26 +1,7 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../routes/route-model');
-
-function protected(req, res, next) {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  } else {
-    res.status(400).json({ message: 'Please provide credentials' });
+module.exports = (req, res, next) => {
+  if(req.session && req.session.username) {
+    next();
+  }else{
+    res.status(401).json({ message: 'Please log in to access this resource' });
   }
 }
-
-module.exports = protected;
